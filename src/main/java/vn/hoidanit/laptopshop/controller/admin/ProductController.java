404dd90c9675771f6d9cd9f3aacbeb6e7aa4baf4
ineedatoothbrush <daod1278@gmail.com>
2025-09-22 20:32:31 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-    @GetMapping("/admin")
-    public String DashBoard() {
-        return "/admin/dashboard/show";
+public class ProductController {
+    @GetMapping("/admin/product")
+    public String Product() {
+        return "/admin/product/show";
     }
 }
