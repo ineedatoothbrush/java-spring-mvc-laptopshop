b7525624c7d72f392554251f9ba43c9ac1d453c5
ineedatoothbrush <daod1278@gmail.com>
2025-09-29 17:53:45 +0700

    .

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,14 +1,26 @@
 package vn.hoidanit.laptopshop.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller
 public class ItemController {
-    @RequestMapping("/product")
-    public String getProductDetailPage(Model model) {
+
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductDetailPage(Model model, @PathVariable long id) {
+        Product products = this.productService.getProductById(id);
+        model.addAttribute("product", products);
         return "/client/product/detail";
     }
 }
